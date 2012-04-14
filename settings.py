309db27ac7c48acd610a1931513ac1b9e5d8a854
ymcagodme <ymcagodme@gmail.com>
2012-04-13 17:48:37 -0700

    Change the settings.py to fix the work path problem.

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-# Django settings for norwalk_tournament project.
+import os
+WORKING_PATH = os.path.dirname(os.path.abspath(__file__))
+print WORKING_PATH
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/ymcagodme/Django-project/norwalk_tournament/judo.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'tournament.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -100,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'norwalk_tournament.urls'
+ROOT_URLCONF = 'Judo-Tournament-Hoster.urls'
 
 TEMPLATE_DIRS = (
-    "/Users/ymcagodme/Django-project/norwalk_tournament/templates"
+    WORKING_PATH + "/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
